d1c2f0064f0e90a92dd25afd79f93df99247e6c8
Le Minh Tri <ansidev@gmail.com>
2022-02-20 15:36:44 +0700

    feat(package): add internal package constant

---
+++ app/constant/app.go
@@ -0,0 +1,13 @@
+package constant
+
+const (
+	DefaultProdEnv    = "prod"
+	DefaultProdConfig = "app.env.example"
+	DefaultDevConfig  = "app.env"
+
+	AppName        = "Gin Starter Project"
+	AppVersion     = "1.0.0"
+	AppReleaseDate = "2022.02.14"
+
+	DateTimeFormat = "2006-01-02 15:04:05"
+)
