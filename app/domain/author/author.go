b44ebba63a95289b263eff3befe0fd6050c86656
Le Minh Tri <ansidev@gmail.com>
2022-02-25 12:25:41 +0700

    fix: correct type for models

--- app/domain/author/author.go
+++ app/domain/author/author.go
@@ -1,10 +1,12 @@
 package author
 
+import "time"
+
 type Author struct {
-	ID        int64  `json:"id"`
-	Name      string `json:"name"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (m Author) TableName() string {
--- app/domain/post/post.go
+++ app/domain/post/post.go
@@ -10,7 +10,7 @@ type Post struct {
 	ID        int64  `json:"id"`
 	Title     string `json:"title"`
 	Content   string `json:"content"`
-	AuthorID  int
+	AuthorID  int64
 	Author    author.Author `json:"author"`
 	CreatedAt time.Time     `json:"created_at"`
 	UpdatedAt time.Time     `json:"updated_at"`
